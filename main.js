c5d4c81d04f2258f43603a285153afd7e0e75291
HowUnfortunate5 <138226411+HowUnfortunate5@users.noreply.github.com>
2025-12-24 09:35:06 +0000

    Have fixed the logic in the game. Had to adjust the scope of variables in order to get the game to work properly

--- main.js
+++ main.js
@@ -2,36 +2,37 @@
 const log = console.log
 
 //  use math.random to get a random number between 1-3 and return it
-function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
-    }
+function getRandomNumber(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-    let computerNumber = (getRandomNumber(3))
+//Create a variable to store the random number
+let computerNumber = (getRandomNumber(1,3))
 
-// create a function that will randomly return rock, paper or scissors using
-// the getRandomNumber as input  
+
+// create a function that will return rock, paper or scissors using
+// the getRandomNumber as input
 
 function getComputerChoice(computerNumber) {
     let result
-    if (computerNumber == 0) {
+    if (computerNumber == 3) {
         result = "rock"; }
 
-    else if (computerNumber == 1) {
+    else if (computerNumber == 2) {
         result = "scissors" ;
-    } else result = "paper";
+    } else if (computerNumber == 1) {
+        return "paper";
+    }
 
     return result;
 }
 
-let computerChoice = getComputerChoice()
-let humanChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase()
-
-
- // Write function compareResults and pass the choice parameters in as arguements
-function compareResults(computerChoice, humanChoice) {
+ // Write function playRound and pass the choice parameters in as arguements
+function playRound(computerChoice, humanChoice) {
     // Create variable to store the answers
    let result
 
+//Using if/else pass the possible results into it and Print the Outcome, storing the value.
 if (humanChoice == "rock" && computerChoice == "rock") {
         result = "It's a Draw";
     } else if (humanChoice == "rock" && computerChoice == "scissors") {
@@ -54,40 +55,66 @@ if (humanChoice == "rock" && computerChoice == "rock") {
 
 }
 
-log(compareResults(computerChoice, humanChoice))
+
+
+
+//Create variables to store the scores of the game
+let computerScore = 0
+let humanScore = 0
+let draw = 0
+
+//Create a function called playGame that updates the scoreboard
+//using If logic and an array. If "V" for victory is the first letter-
+//-of result, human wins. And "D" for Defeat means computer wins
+function playGame() {
+
+// Create variables to store player and computer answers  
+
+let computerChoice = getComputerChoice(computerNumber)
+let humanChoice = prompt("Choose Rock, Paper or Scissors").toLowerCase()
+
+// create variable to store the result of the playRound function
+let result = playRound(computerChoice, humanChoice)
+
+//Using an array I will return the first letter of the result, which will-
+//-indicate whether Player won, lost or drew.
+if (result[0] == "V") {
+        humanScore+= 1;
+    } else if (result[0] == "D") {
+        computerScore+= 1;
+      }  else draw++;
+
+//Print the scores, ect
+log ("Computer Choice: " + computerChoice)
+log ("Human Choice: " + humanChoice)
+log (result)
+log ("Score: Human " + humanScore + " " + "Computer " + computerScore)
+    }
+
+
+//make everything happen 5 times 
+for (let round = 0; round < 5; round++ ) {
+    playGame()
+    //Print the round number 
+    log ("Round: " + (round+1))
+}
+
+
+
+//Create a function that takes gameResult, and using an array-
+//-will return who won the game. 
+
+//if the first letter of gameResult is equal to "V" for Victory, the human-
+//-has won the game. "D" for Defeat means the computer one.
+
+//The function will return eiter
+
+
+
+
  
 
-    
-function playGame() {
-// create two variables to keep the score of the game
-const humanScore = 0
-const computerScore = 0
-const round = 0
-const roundsRemaining = (5 - round)
-let = score
-while (keepGoing = True)
 
-// create a variable called score that turns the playRound result into a variable
-score = playRound(humanChoice, computerChoice)
-
-    if (score[0] == "V") {
-        humanScore++;
-    } else if (score[0] == "D") {
-        computerScore++;
-    }
-    // Increase the number of rounds played each time
-    round++
-
-    log("Computer Choice: " + computerChoice + " score: " + computerScore)
-    log("Player Choice: " + humanChoice + " score: " + humanScore)
-    log(result)
-    log("Rounds remaining: " + roundsRemaining )
-
-    //Break at round 5
-    if (round == 5) {
-        keepGoing == false;
-    }
-}
 
 
 // Play 5 rounds by calling playRound 5 times.
